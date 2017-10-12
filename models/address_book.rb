3cc0b0522035a4f42b493c90be9d99e8792dab12
Ross Ginn <ross.ginn@gmail.com>
2017-10-12 00:12:38 -0600

    address bloc csv 2 errors

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,35 +1,47 @@
 require_relative 'entry'
 
+require "csv"
+
 class AddressBook
-  attr_reader :entries
+attr_reader :entries
 
   def initialize
-     @entries = []
+    @entries = []
   end
 
   def remove_entry(name, phone, email)
     delete_entry = nil
-
     @entries.each do |entry|
       if name == entry.name && phone == entry.phone_number && email == entry.email
         delete_entry = entry
       end
-    end
-
-    @entries.delete(delete_entry)
   end
 
+  @entries.delete(delete_entry)
+end
+
   def add_entry(name, phone_number, email)
-    # #9
+# #9
     index = 0
     entries.each do |entry|
-    # #10
+# #10
       if name < entry.name
         break
       end
     index+= 1
   end
-    # #11
-    entries.insert(index, Entry.new(name, phone_number, email))
+# #11
+  entries.insert(index, Entry.new(name, phone_number, email))
+  end
+
+  def import_from_csv(file_name)
+# Implementation goes here
+    csv_text = File.read(file_name)
+    csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+# #8
+    csv.each do |row|
+      row_hash = row.to_hash
+      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+    end
   end
 end
